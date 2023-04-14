d353cb0db3f315be190a2b3248ff5bea74dd2eec
nikita <nikita.surzhin@gmail.com>
2023-04-14 21:13:44 +0300

    testing of checkbox has been implemented

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,5 +1,5 @@
 import time
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElement:
@@ -14,3 +14,15 @@ class TestElement:
             assert email == output_email, "email name does not match"
             assert current_address == output_cur_addr, "current address does not match"
             assert permanent_address == output_per_addr, "permanent address the full name does not match"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            print(input_checkbox)
+            print(output_result)
+            assert input_checkbox == output_result, 'checkboxes have not been selected'
