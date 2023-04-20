93305f4724d60973fb2433d76b19011a1a62a778
nikita <nikita.surzhin@gmail.com>
2023-04-20 21:11:18 +0300

    added test: - test_different_click_on_the_buttons

--- conftest.py
+++ conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     driver.maximize_window()
     yield driver
     driver.quit()
